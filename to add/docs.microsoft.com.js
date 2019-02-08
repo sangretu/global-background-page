6281a176d0c48e0bf6017bca0c6a14fe3075c2f4
sangretu <github@geeq.com>
2019-02-08 15:46:16 -0800

    added 'to add' folder

---
+++ to add/docs.microsoft.com.js
@@ -0,0 +1,19 @@
+/**
+ * 20180721
+ * 
+ * Removing fluff from bot framework docs for printing
+ * https://docs.microsoft.com/en-us/bot-service/dotnet/...
+ **/
+
+$('.header-holder').remove()
+$('.content-header').remove()
+$('.pageActions').remove()
+$('.moniker-picker').remove()
+$('.page-metadata').remove()
+$('#sidebar').remove()
+$('#center-doc-outline').remove()
+$('#openFeedbackContainer').remove()
+$('.footerContainer').remove()
+$('.action-panel').remove()
+$('.feedback-system-change-alert').remove()
+$('#comments-container').remove()
